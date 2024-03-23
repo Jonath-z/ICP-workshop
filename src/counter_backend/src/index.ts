6f0b4d27889da5cc1b08ba54bd7a9e2e66320927
Jonath-z <jonathanzihindula95@gmail.com>
2024-03-23 19:11:26 +0200

    chore: add canister testing with azle

--- src/counter_backend/src/index.ts
+++ src/counter_backend/src/index.ts
@@ -1,7 +1,21 @@
-import { Canister, query, text } from 'azle';
+import { Canister, StableBTreeMap, init, nat8, update } from "azle";
+
+// let counter = 0;
+
+let counter = StableBTreeMap<string, number>(0);
 
 export default Canister({
-    greet: query([text], text, (name) => {
-        return `Hello, ${name}!`;
-    })
-})
+  init: init([], () => {
+    counter.insert("counter", 0);
+  }),
+
+  increment: update([], nat8, () => {
+    let currentCount = counter.get("counter").Some;
+    if (currentCount !== undefined) {
+      const updatedCount = currentCount + 3;
+      counter.insert("counter", updatedCount);
+      return updatedCount;
+    }
+    return 0;
+  }),
+});
